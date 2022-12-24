7718aecdf751821f3142c4fcbd157e7c9a8e6305
wh0ois <121345613+wh0ois@users.noreply.github.com>
2022-12-24 22:14:15 +0530

    Update codec.py
    In encode(), timestamp is converted to 8 byte float, and K, V size are bot converted to 8 byte integers. The K & V are encoded to ascii.
    
    decode() does the reverse using struct.unpack()

--- kvdbpy/codec.py
+++ kvdbpy/codec.py
@@ -10,3 +10,33 @@ In encode(), timestamp is converted to 8 byte float, and K, V size are bot conve
 
 decode() does the reverse using struct.unpack() 
 """
+
+import struct
+from collections import namedtuple
+import kvdb_file
+
+METADATA_STRUCT = ">dqq" #a byte layput of a double 'd', 2 long longs 'q' in big endian format '>'
+METADATA_BYTE_SIZE = 24
+
+def encode(record):
+    key_size = record.keysize
+    value_size = record.valuesize
+    timestamp = record.timestamp
+    key = record.key
+    value = record.value
+
+    metadata = struct.pack(METADATA_STRUCT, timestamp, key_size, value_size)
+    data = key.encode() + value.encode()
+    record_bytes = metadata + data
+    return record_bytes
+
+def decode_metadata(data):
+    (timestamp, keysize, valuesize) = struct.unpack(METADATA_STRUCT, data)
+    return (timestamp, keysize, valuesize)
+
+def decode(data):
+    (timestamp, ksize, vsize) = decode_metadata(data[:METADATA_BYTE_SIZE])
+    string_data = data[METADATA_BYTE_SIZE:]
+    key = string_data[:ksize]
+    value = string_data[ksize:]
+    return kvdb_file.Record(timestamp, ksize, vsize, key, value)
